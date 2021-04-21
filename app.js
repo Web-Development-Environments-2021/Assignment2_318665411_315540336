4dd0a6baba919bd9a604c9aad875bdf076673c1f
roi <roirein@post.bgu.ac.il>
2021-04-21 21:01:00 +0300

    settings

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ let start_time;
 let time_elapsed;
 let interval;
 let users = []
+let options=['1','2','3','4','5','6','7','8','9','0','a','b','c',
+		'd','c','d','e','f','g','h','i','j','k','l','m','n',
+		'o','p','q','s','t','u','v','w','x','y','z','Arrowup',
+		'Arrowdown','Arrowleft','Arrowright' ]
 
 
 class User{
@@ -20,242 +24,242 @@ class User{
 	}
 }
 
- $(document).ready(function() {
-	 let k_user = new User('k','k','k','k@gmail.com','k','1/1/1970')
-	 users.push(k_user)
-	 context = (document.getElementById('canvas')).getContext("2d");
-	 $.validator.addMethod("lettersonly", function(value, element) {
-		 return this.optional(element) || /^[a-zA-Z ]*$/.test(value);
-	 });
-	 $.validator.addMethod("lettersanddigits", function(value, element) {
-		 return this.optional(element) || /^(?=.*[A-Za-z])(?=.*\d)/.test(value);
-	 });
-	 $("#register_form").validate({
-			 rules: {
-				 uname: {
-					 required: true,
-				 },
-				 fname: {
-					 required: true,
-					 lettersonly: true
-				 },
-				 lname: {
-					 required: true,
-					 lettersonly: true
-				 },
-				 email: {
-					 required: true,
-					 email: true
-				 },
-				 password: {
-					 required: true,
-					 minlength: 6,
-					 lettersanddigits: true
-				 },
-				 datepicker: {
-					 required: true
-				 }
-			 },
-			 messages: {
-				 uname: {
-					 required: "Please enter your username"
-				 },
-				 fname: {
-					 required: "Please enter your first name",
-					 lettersonly: "First name should include letters only"
-				 },
-				 lname: {
-					 required: "Please enter your last name",
-					 lettersonly: "Last name should include letters only"
-				 },
-				 email: {
-					 required: "Please enter your email",
-					 email: "The email should be in the format: example@example.com"
-				 },
-				 password: {
-					 required: "Please enter a password",
-					 minlength: "Password should be at least 6 characters",
-					 lettersanddigits: "Password should include both letters and digits"
-				 },
-				 datepicker: {
-					 required: "Please choose your date of birth"
-				 }
-			 }
-		 })
+$(document).ready(function() {
+	let k_user = new User('k','k','k','k@gmail.com','k','1/1/1970')
+	users.push(k_user)
+	context = (document.getElementById('canvas')).getContext("2d");
+	$.validator.addMethod("lettersonly", function(value, element) {
+		return this.optional(element) || /^[a-zA-Z ]*$/.test(value);
+	});
+	$.validator.addMethod("lettersanddigits", function(value, element) {
+		return this.optional(element) || /^(?=.*[A-Za-z])(?=.*\d)/.test(value);
+	});
+	$("#register_form").validate({
+		rules: {
+			uname: {
+				required: true,
+			},
+			fname: {
+				required: true,
+				lettersonly: true
+			},
+			lname: {
+				required: true,
+				lettersonly: true
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			password: {
+				required: true,
+				minlength: 6,
+				lettersanddigits: true
+			},
+			datepicker: {
+				required: true
+			}
+		},
+		messages: {
+			uname: {
+				required: "Please enter your username"
+			},
+			fname: {
+				required: "Please enter your first name",
+				lettersonly: "First name should include letters only"
+			},
+			lname: {
+				required: "Please enter your last name",
+				lettersonly: "Last name should include letters only"
+			},
+			email: {
+				required: "Please enter your email",
+				email: "The email should be in the format: example@example.com"
+			},
+			password: {
+				required: "Please enter a password",
+				minlength: "Password should be at least 6 characters",
+				lettersanddigits: "Password should include both letters and digits"
+			},
+			datepicker: {
+				required: "Please choose your date of birth"
+			}
+		}
+	})
 
-	 $("#login_form").validate({
-		 rules:{
-		 	uname:{
-		 		required:true,
+	$("#login_form").validate({
+		rules:{
+			uname:{
+				required:true,
 			},
 			password:{
-		 		required:true,
+				required:true,
 			}
-		 }
-	 })
-
- 	Start();
- });
+		}
+	})
+	load_key_options()
+	Start();
+});
 
 
 function Start() {
- 	board = new Array();
- 	score = 0;
- 	pac_color = "blue";
- 	let cnt = 100;
- 	let food_remain = 50;
- 	let pacman_remain = 1;
- 	start_time = new Date();
- 	for (let i = 0; i < 10; i++) {
- 		board[i] = new Array();
- 		//put obstacles in (i=3,j=3) and (i=3,j=4) and (i=3,j=5), (i=6,j=1) and (i=6,j=2)
- 		for (let j = 0; j < 10; j++) {
- 			if (
- 				(i == 3 && j == 3) ||
- 				(i == 3 && j == 4) ||
- 				(i == 0 && j == 0) ||
- 				(i == 6 && j == 1) ||
- 				(i == 6 && j == 2)
- 			) {
- 				board[i][j] = 4;
- 			} else {
- 				let randomNum = Math.random();
- 				if (randomNum <= (1.0 * food_remain) / cnt) {
- 					food_remain--;
+	board = new Array();
+	score = 0;
+	pac_color = "blue";
+	let cnt = 100;
+	let food_remain = 50;
+	let pacman_remain = 1;
+	start_time = new Date();
+	for (let i = 0; i < 10; i++) {
+		board[i] = new Array();
+		//put obstacles in (i=3,j=3) and (i=3,j=4) and (i=3,j=5), (i=6,j=1) and (i=6,j=2)
+		for (let j = 0; j < 10; j++) {
+			if (
+				(i == 3 && j == 3) ||
+				(i == 3 && j == 4) ||
+				(i == 0 && j == 0) ||
+				(i == 6 && j == 1) ||
+				(i == 6 && j == 2)
+			) {
+				board[i][j] = 4;
+			} else {
+				let randomNum = Math.random();
+				if (randomNum <= (1.0 * food_remain) / cnt) {
+					food_remain--;
 					board[i][j] = 1;
 				} else if (randomNum < (1.0 * (pacman_remain + food_remain)) / cnt) {
- 					shape.i = i;
- 					shape.j = j;
+					shape.i = i;
+					shape.j = j;
 					pacman_remain--;
- 					board[i][j] = 2;
- 				} else {
- 					board[i][j] = 0;
- 				}
- 				cnt--;
- 			}
- 		}
- 	}
- 	while (food_remain > 0) {
- 		let emptyCell = findRandomEmptyCell(board);
- 		board[emptyCell[0]][emptyCell[1]] = 1;
- 		food_remain--;
- 	}
- 	keysDown = {};
- 	addEventListener(
- 		"keydown",
- 		function(e) {
- 			keysDown[e.keyCode] = true;
- 		},
- 		false
- 	);
- 	addEventListener(
- 		"keyup",
- 		function(e) {
- 			keysDown[e.keyCode] = false;
- 		},
- 		false
- 	);
- 	interval = setInterval(UpdatePosition, 100);
- }
-
- function findRandomEmptyCell(board) {
- 	let i = Math.floor(Math.random() * 9 + 1);
- 	let j = Math.floor(Math.random() * 9 + 1);
- 	while (board[i][j] != 0) {
- 		i = Math.floor(Math.random() * 9 + 1);
- 		j = Math.floor(Math.random() * 9 + 1);
- 	}
- 	return [i, j];
- }
-
- function GetKeyPressed() {
- 	if (keysDown[38]) {
- 		return 1;
- 	}
- 	if (keysDown[40]) {
- 		return 2;
- 	}
- 	if (keysDown[37]) {
- 		return 3;
- 	}
- 	if (keysDown[39]) {
- 		return 4;
- 	}
- }
-
- function Draw() {
- 	canvas.width = canvas.width; //clean board
- 	lblScore.value = score;
- 	lblTime.value = time_elapsed;
- 	for (let i = 0; i < 10; i++) {
- 		for (let j = 0; j < 10; j++) {
- 			let center = new Object();
- 			center.x = i * 60 + 30;
- 			center.y = j * 60 + 30;
- 			if (board[i][j] == 2) {
- 				context.beginPath();
- 				context.arc(center.x, center.y, 30, 0.15 * Math.PI, 1.85 * Math.PI); // half circle
- 				context.lineTo(center.x, center.y);
- 				context.fillStyle = pac_color; //color
- 				context.fill();
- 				context.beginPath();
- 				context.arc(center.x + 5, center.y - 15, 5, 0, 2 * Math.PI); // circle
- 				context.fillStyle = "black"; //color
- 				context.fill();
- 			} else if (board[i][j] == 1) {
- 				context.beginPath();
- 				context.arc(center.x, center.y, 15, 0, 2 * Math.PI); // circle
- 				context.fillStyle = "black"; //color
- 				context.fill();
- 			} else if (board[i][j] == 4) {
- 				context.beginPath();
- 				context.rect(center.x - 30, center.y - 30, 60, 60);
- 				context.fillStyle = "grey"; //color
- 				context.fill();
- 			}
- 		}
+					board[i][j] = 2;
+				} else {
+					board[i][j] = 0;
+				}
+				cnt--;
+			}
+		}
 	}
- }
+	while (food_remain > 0) {
+		let emptyCell = findRandomEmptyCell(board);
+		board[emptyCell[0]][emptyCell[1]] = 1;
+		food_remain--;
+	}
+	keysDown = {};
+	addEventListener(
+		"keydown",
+		function(e) {
+			keysDown[e.keyCode] = true;
+		},
+		false
+	);
+	addEventListener(
+		"keyup",
+		function(e) {
+			keysDown[e.keyCode] = false;
+		},
+		false
+	);
+	interval = setInterval(UpdatePosition, 100);
+}
 
- function UpdatePosition() {
- 	board[shape.i][shape.j] = 0;
- 	var x = GetKeyPressed();
- 	if (x == 1) {
- 		if (shape.j > 0 && board[shape.i][shape.j - 1] != 4) {
- 			shape.j--;
- 		}
- 	}
- 	if (x == 2) {
- 		if (shape.j < 9 && board[shape.i][shape.j + 1] != 4) {
- 			shape.j++;
- 		}
- 	}
- 	if (x == 3) {
- 		if (shape.i > 0 && board[shape.i - 1][shape.j] != 4) {
- 			shape.i--;
- 		}
- 	}
- 	if (x == 4) {
- 		if (shape.i < 9 && board[shape.i + 1][shape.j] != 4) {
- 			shape.i++;
- 		}
- 	}
- 	if (board[shape.i][shape.j] == 1) {
- 		score++;
- 	}
- 	board[shape.i][shape.j] = 2;
- 	let currentTime = new Date();
- 	time_elapsed = (currentTime - start_time) / 1000;
- 	if (score >= 20 && time_elapsed <= 10) {
- 		pac_color = "green";
- 	}
- 	if (score == 50) {
- 		window.clearInterval(interval);
- 		window.alert("Game completed");
- 	} else {
- 		Draw();
- 	}
- }
+function findRandomEmptyCell(board) {
+	let i = Math.floor(Math.random() * 9 + 1);
+	let j = Math.floor(Math.random() * 9 + 1);
+	while (board[i][j] != 0) {
+		i = Math.floor(Math.random() * 9 + 1);
+		j = Math.floor(Math.random() * 9 + 1);
+	}
+	return [i, j];
+}
+
+function GetKeyPressed() {
+	if (keysDown[38]) {
+		return 1;
+	}
+	if (keysDown[40]) {
+		return 2;
+	}
+	if (keysDown[37]) {
+		return 3;
+	}
+	if (keysDown[39]) {
+		return 4;
+	}
+}
+
+function Draw() {
+	canvas.width = canvas.width; //clean board
+	lblScore.value = score;
+	lblTime.value = time_elapsed;
+	for (let i = 0; i < 10; i++) {
+		for (let j = 0; j < 10; j++) {
+			let center = new Object();
+			center.x = i * 60 + 30;
+			center.y = j * 60 + 30;
+			if (board[i][j] == 2) {
+				context.beginPath();
+				context.arc(center.x, center.y, 30, 0.15 * Math.PI, 1.85 * Math.PI); // half circle
+				context.lineTo(center.x, center.y);
+				context.fillStyle = pac_color; //color
+				context.fill();
+				context.beginPath();
+				context.arc(center.x + 5, center.y - 15, 5, 0, 2 * Math.PI); // circle
+				context.fillStyle = "black"; //color
+				context.fill();
+			} else if (board[i][j] == 1) {
+				context.beginPath();
+				context.arc(center.x, center.y, 15, 0, 2 * Math.PI); // circle
+				context.fillStyle = "black"; //color
+				context.fill();
+			} else if (board[i][j] == 4) {
+				context.beginPath();
+				context.rect(center.x - 30, center.y - 30, 60, 60);
+				context.fillStyle = "grey"; //color
+				context.fill();
+			}
+		}
+	}
+}
+
+function UpdatePosition() {
+	board[shape.i][shape.j] = 0;
+	var x = GetKeyPressed();
+	if (x == 1) {
+		if (shape.j > 0 && board[shape.i][shape.j - 1] != 4) {
+			shape.j--;
+		}
+	}
+	if (x == 2) {
+		if (shape.j < 9 && board[shape.i][shape.j + 1] != 4) {
+			shape.j++;
+		}
+	}
+	if (x == 3) {
+		if (shape.i > 0 && board[shape.i - 1][shape.j] != 4) {
+			shape.i--;
+		}
+	}
+	if (x == 4) {
+		if (shape.i < 9 && board[shape.i + 1][shape.j] != 4) {
+			shape.i++;
+		}
+	}
+	if (board[shape.i][shape.j] == 1) {
+		score++;
+	}
+	board[shape.i][shape.j] = 2;
+	let currentTime = new Date();
+	time_elapsed = (currentTime - start_time) / 1000;
+	if (score >= 20 && time_elapsed <= 10) {
+		pac_color = "green";
+	}
+	if (score == 50) {
+		window.clearInterval(interval);
+		window.alert("Game completed");
+	} else {
+		Draw();
+	}
+}
 
 function go_to_reg(){
 	$("body").css("background-color","rgb(30,30,40)")
@@ -287,9 +291,9 @@ function validate_vals(){
 	else{
 		$("#Login").hide()
 		$("#Login").css("visibility","hidden")
-		$("#Game_Area").show()
+		$("#settings").show()
 		$("body").css("background-color","white")
-		$("#Game_Area").css("visibility","visible")
+		$("#settings").css("visibility","visible")
 	}
 	return false
 }
@@ -309,3 +313,28 @@ function add_user(){
 	return false
 }
 
+
+function load_key_options(){
+	let up = document.getElementById('upkey')
+	let down = document.getElementById('downkey')
+	let right = document.getElementById('rightkey')
+	let left = document.getElementById('leftkey')
+	options.forEach((val,ind)=>{
+		let opt1 = document.createElement('option')
+		let opt2 = document.createElement('option')
+		let opt3 = document.createElement('option')
+		let opt4 = document.createElement('option')
+		opt1.text = val;
+		opt1.value = ind;
+		opt2.text = val;
+		opt2.value = ind;
+		opt3.text = val;
+		opt3.value = ind;
+		opt4.text = val;
+		opt4.value = ind;
+		up.appendChild(opt1);
+		down.appendChild(opt2);
+		right.appendChild(opt3);
+		left.appendChild(opt4);
+	})
+}
